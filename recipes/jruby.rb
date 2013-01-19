f487a1dc815129813191389532e6a87c8a80583f
Jon Doveston <jon@doveston.me.uk>
2013-01-19 12:21:09 +0000

    fixed

--- recipes/jruby.rb
+++ recipes/jruby.rb
@@ -7,30 +7,35 @@ directory '/usr/local/stow' do
   action :create
 end
 
-ruby_build_ruby 'jruby-1.7.1' do
+jruby = 'jruby-1.7.1'
+
+ruby_build_ruby jruby do
   action :install
-  prefix_path '/usr/local/stow'
+  prefix_path "/usr/local/stow/#{jruby}"
 end
 
 execute 'stow jruby' do
-  command 'stow -R -d /usr/local/stow -t /usr/local jruby-1.7.1'
+  command "stow -R -d /usr/local/stow -t /usr/local #{jruby}"
   user 'root'
+  group 'root'
 end
 
 execute 'update rubygems' do
-  command '/usr/local/stow/jruby-1.7.1/bin/gem update --system'
+  command "/usr/local/stow/#{jruby}/bin/gem update --system"
   user 'root'
+  group 'root'
 end
 
 %w[bundler].each do |gem_name|
   gem_package gem_name do
-    gem_binary '/usr/local/stow/jruby-1.7.1/bin/gem'
+    gem_binary "/usr/local/stow/#{jruby}/bin/gem"
     options '--no-ri --no-rdoc'
     action :install
   end
 end
 
 execute 'restow jruby' do
-  command 'stow -R -d /usr/local/stow -t /usr/local jruby-1.7.1'
+  command "stow -R -d /usr/local/stow -t /usr/local #{jruby}"
   user 'root'
+  group 'root'
 end
