7ac96be691707081988ea565ee4560dc4cd96e76
Jon Doveston <jon@doveston.me.uk>
2013-01-23 20:20:52 +0000

    Update recipes/rails.rb

--- recipes/rails.rb
+++ recipes/rails.rb
@@ -1,5 +1,5 @@
 %w[
-  nodejs imagemagick libproj-dev proj libxml2-dev libxslt1-dev
+  nodejs imagemagick libproj-dev proj libxml2-dev libxslt1-dev sqlite3 libsqlite3-dev
 ].each do |name|
   package name do
     action :install
