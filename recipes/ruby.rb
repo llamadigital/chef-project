c5dca35fef3f97b8a8f7c48ad37e92271e0ac6ed
Jon Doveston <jon@doveston.me.uk>
2013-02-26 20:28:03 +0000

    Update recipes/ruby.rb

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -2,7 +2,7 @@ package 'ruby1.9.1-full'
 package 'ruby1.9.1-dev'
 
 execute 'update rubygems' do
-  command '/usr/bin/gem update --system'
+  command '/usr/bin/gem update --system 1.8.25'
   user 'root'
   environment 'REALLY_GEM_UPDATE_SYSTEM' => 'true'
 end
