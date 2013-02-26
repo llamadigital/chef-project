6ce0d926e5c97b3d5a91da35cbfc55b997fb31b5
Jon Doveston <jon@doveston.me.uk>
2013-02-26 13:07:05 +0000

    Update recipes/rails.rb

--- recipes/rails.rb
+++ recipes/rails.rb
@@ -1,7 +1,12 @@
 %w[
-  nodejs imagemagick libproj-dev proj libxml2-dev libxslt1-dev sqlite3 libsqlite3-dev libzmq-dev
+  imagemagick libproj-dev libxml2-dev libxslt1-dev sqlite3 libsqlite3-dev libzmq-dev
 ].each do |name|
-  package name do
-    action :install
-  end
+  package name
+end
+
+case node['platform']
+when 'ubuntu'
+  package 'nodejs'
+when 'debian'
+  # TODO js runtime or nodejs from somewhere
 end
