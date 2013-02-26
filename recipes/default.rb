752c24d28beb37ed80c24c62f73d8bbf1e39addb
Jon Doveston <jon@doveston.me.uk>
2013-02-26 13:12:34 +0000

    Update recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,7 +9,13 @@
 include_recipe 'project::set_locale'
 
 include_recipe 'apt'
-include_recipe 'ubuntu'
+
+case node['platform']
+when 'ubuntu'
+  include_recipe 'ubuntu'
+when 'debian'
+end
+
 include_recipe 'users::sysadmins'
 # include_recipe 'sudo' # TODO: this is having a problem with the config
 
