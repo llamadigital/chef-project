22f4591be8fb26effdf67d4ec94b776ffa22ce2e
Jon Doveston <jon@doveston.me.uk>
2013-02-26 20:33:54 +0000

    Update recipes/ruby.rb

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -7,9 +7,8 @@ execute 'update rubygems' do
   environment 'REALLY_GEM_UPDATE_SYSTEM' => 'true'
 end
 
-%w[bundler].each do |gem_name|
-  gem_package gem_name do
-    gem_binary '/usr/bin/gem'
-    action :install
-  end
+gem_package gem_name do
+  gem_binary '/usr/bin/gem'
+  version '1.2.5'
+  action :install
 end
