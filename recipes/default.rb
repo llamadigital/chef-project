8215292cb3dc6abe0daac83881f5388652b028da
Jon Doveston <jon@doveston.me.uk>
2013-02-28 16:18:42 +0000

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,15 +7,13 @@
 # All rights reserved - Do Not Redistribute
 
 include_recipe 'apt'
+include_recipe 'build-essential'
 
 case node['platform']
 when 'ubuntu'
   include_recipe 'project::set_locale'
   include_recipe 'ubuntu'
+  include_recipe 'users::sysadmins'
+  # include_recipe 'sudo' # TODO: this is having a problem with the config
 when 'debian'
 end
-
-include_recipe 'users::sysadmins'
-# include_recipe 'sudo' # TODO: this is having a problem with the config
-
-include_recipe 'build-essential'
