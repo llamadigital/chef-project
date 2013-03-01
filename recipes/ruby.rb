932aa66b4dbcaf5b05f2fb9b84cfb02cda623735
Jon Doveston <jon@doveston.me.uk>
2013-03-01 14:44:56 +0000

    merge

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,14 +1,13 @@
 package 'ruby1.9.1-full'
 package 'ruby1.9.1-dev'
 
-execute 'update rubygems' do
-  command '/usr/bin/gem update --system 1.8.25'
-  user 'root'
-  environment 'REALLY_GEM_UPDATE_SYSTEM' => 'true'
-end
+# execute 'update rubygems' do
+#   command '/usr/bin/gem update --system 1.8.25'
+#   user 'root'
+#   environment 'REALLY_GEM_UPDATE_SYSTEM' => 'true'
+# end
 
 gem_package 'bundler' do
   gem_binary '/usr/bin/gem'
-  version '1.2.5'
   action :install
 end
